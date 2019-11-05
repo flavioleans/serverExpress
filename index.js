b725f0cdb9ea92b5785dc398fc3e5376e9d2f492
flavioleans <leans.flavio@gmail.com>
2019-11-04 23:50:48 -0300

    criacao de servidor Node

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express()
+
+//rota principal
+app.get("/", function(req, res){
+    res.send("Welcome to my aplication server")
+})
+
+//nova rota
+app.get("/about", function(req, res){
+    res.send("Page About Us")
+})
+
+//nova rota
+app.get("/blog", function(req, res){
+    res.send("Welcome to my Blog")
+})
+
+//rota hello com parametro
+app.get("/hello/:name/:age/:office", function(req, res){
+    res.send("<h1>Ola "+ req.params.name+"</h1>" + "<h3>Age: </h3>"+req.params.age+"<h3> Oficce: "+req.params.office)
+})
+
+app.listen(8081, function(){
+    console.log("Server active in http://localhost:8081")
+})
